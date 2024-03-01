75a32f29323b3a4257bc73d84f837db06ec71a49
Darya Shadrina <d.shadrina@mirtekgroup.ru>
2024-03-01 12:00:57 +0300

    добавила решение второй задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Main();
+void Main()
+{
+    int m = 3;
+    int n = 2;
+    Console.WriteLine($"Значение функции Аккермана при m={m} и n={n}: {AccermamFunc(m, n)}");
+}
+
+int AccermamFunc(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+
+    else //m>0
+    {
+        if (n == 0)
+        {
+            return AccermamFunc(m - 1, 1);
+        }
+        else //n>0
+        {
+            return AccermamFunc(m - 1, AccermamFunc(m, n - 1));
+        }
+    }
+
+}
